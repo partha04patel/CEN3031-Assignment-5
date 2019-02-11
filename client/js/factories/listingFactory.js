7e8ce9e62ad6f1258bff22ad324b6463a5e4c54b
Parth Patel <parth04patel@gmail.com>
2019-02-11 18:01:27 -0500

    changed factory stuff

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -4,21 +4,22 @@ angular.module('listings', []).factory('Listings', function($http) {
          /*
        returns result of HTTP GET Request with given listing to route specified by Express.
        */
-      return $http.get('http://localhost:8080/api/listings');
+     // return $http.get('http://localhost:8080/api/listings');
+     return $http.post('https://tranquil-gorge-24478.herokuapp.com/api/listings', listing); 
     },
 	
 	create: function(listing) {
     /*
        returns result of HTTP POST Request with given listing to route specified by Express.
        */
-	  return $http.post('http://localhost:8080/api/listings', listing);
+	  return $http.post('https://tranquil-gorge-24478.herokuapp.com/api/listings', listing);
     }, 
 
     delete: function(id) {
 	   /**TODO
         return result of HTTP delete method with given listing to route specified by Express.
        */
-      return $http.delete('http://localhost:8080/api/listings/' + id);
+      return $http.delete('https://tranquil-gorge-24478.herokuapp.com/api/listings/' + id);
 
     }
   };
